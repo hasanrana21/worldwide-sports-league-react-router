39c8e9440ef951ba1cd73a2a1ba3b6eab1c7254f
hasan178821 <hasanrana178821@gmail.com>
2021-03-11 03:37:09 +0600

    solved err

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -1,12 +1,12 @@
 import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Social.css';
 
 const Social = (props) => {
-    console.log(props.socialLink);
-    const {strTwitter, strFacebook, strYoutube} = props.socialLink;
+    // console.log(props.socialLink);
+    // const {strTwitter, strFacebook, strYoutube} = props.socialLink;
     return (
         <div className="social-icon">
             <a href="https://www.facebook.com" target="blank"><FontAwesomeIcon icon={faFacebook}/></a>
